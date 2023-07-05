c1c4c17cfab36bb1b314c9c5727569bf38d9e65f
Chris Johnson <chris.johnson@senecacollege.ca>
2023-07-05 13:04:53 -0400

    Initial course code change.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,19 +5,19 @@ const katex = require('rehype-katex');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'OER Course Title',
-  tagline: 'OER Tag/Keyword',
+  title: 'SEC520 - Internet Security',
+  tagline: 'SEC520',
   url: 'https://Seneca-ICTOER.github.io/',
-  baseUrl: '/OERTemplate/',
+  baseUrl: '/SEC520/',
   trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
   organizationName: 'Seneca-ICTOER',
-  projectName: 'OERTemplate',
+  projectName: 'SEC520',
   themeConfig: {
     navbar: {
-      title: 'OER Course Title',
+      title: 'SEC520 - Internet Security',
       logo: {
         alt: 'Seneca College',
         src: 'img/logo.svg',
@@ -31,7 +31,7 @@ module.exports = {
       style: 'dark',
       links: [
         {
-          title: 'OER Course Title',
+          title: 'SEC520 - Internet Security',
           items: [
             {
               label: 'Contents',
@@ -57,7 +57,7 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           routeBasePath: '/',
-          editUrl: 'https://github.com/Seneca-ICTOER/OERTemplate/tree/main',
+          editUrl: 'https://github.com/Seneca-ICTOER/SEC520/tree/main',
           remarkPlugins: [math],
           rehypePlugins: [katex],
         },
